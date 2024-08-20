57bb2e674d664fd83836149049fb487d84a7ec11
AngelMrt <angelmrtnic@gmail.com>
2024-08-20 15:56:59 -0600

    Iniciando con la programacion de las paginas publicas

---
+++ views/paginas/conferencias.php
@@ -0,0 +1,22 @@
+<main class="agenda">
+    <h2 class="agenda__heading">Workshops & Conferencias</h2>
+    <p class="agenda__descripcion">Talleres y conferencias dictadas por expertos en Desarrollo Web</p>
+
+    <div class="eventos">
+        <h3 class="eventos__heading">&lt;Conferencias/></h3>
+        <p class="eventos__fecha">Viernes 5 de Octubre</p>
+        <div class="eventos__listado"></div>
+
+        <p class="eventos__fecha">Sabado 6 de Octubre</p>
+        <div class="eventos__listado"></div>
+    </div>
+
+    <div class="eventos eventos__workshops">
+        <h3 class="eventos__heading">&lt;Workshops/></h3>
+        <p class="eventos__fecha">Viernes 5 de Octubre</p>
+        <div class="eventos__listado"></div>
+
+        <p class="eventos__fecha">Sabado 6 de Octubre</p>
+        <div class="eventos__listado"></div>
+    </div>
+</main>
---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,21 @@
+<main class="devwebcamp">
+    <h2 class="devwebcamp__heading"> <?php echo $titulo; ?></h2>
+    <p class="devwebcamp__descripcion">Conoce la conferencia mas importante de Latinoamérica</p>
+    <div class="devwebcamp__grid">
+        <div class="devwebcamp__imagen">
+            <picture>
+                <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+                <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+                <img src="build/img/sobre_devwebcamp.jpg" loading="lazy" width="200" height="300" alt="ImagenDevWebCamp">
+            </picture>
+        </div>
+        <div class="devwebcamp__contenido">
+            <p class="devwebcamp__texto">
+                Ut aliquam elementum massa sed commodo. Fusce posuere, sapien malesuada accumsan placerat, est nulla tempus arcu, id vulputate eros felis eget arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum mattis nec dui in hendrerit. Ut blandit iaculis velit, a volutpat felis varius id. Nulla ullamcorper.
+            </p>
+            <p class="devwebcamp__texto">
+                Ut aliquam elementum massa sed commodo. Fusce posuere, sapien malesuada accumsan placerat, est nulla tempus arcu, id vulputate eros felis eget arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum mattis nec dui in hendrerit. Ut blandit iaculis velit, a volutpat felis varius id. Nulla ullamcorper.
+            </p>
+        </div>
+    </div>
+</main>
